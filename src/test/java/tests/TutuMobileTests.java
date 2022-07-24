fd824c00a69e8c2bbaee6783326922bda1406e31
Natalya Kadysheva <83497921+Native1331@users.noreply.github.com>
2022-07-24 11:58:14 +0300

    Update TutuMobileTests.java

--- src/test/java/tests/TutuMobileTests.java
+++ src/test/java/tests/TutuMobileTests.java
@@ -1,10 +1,7 @@
 package tests;
-
-
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
