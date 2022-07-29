b4db03da059cb2311e319e03bb0da1ba4328d857
Natalya Kadysheva <83497921+Native1331@users.noreply.github.com>
2022-07-29 23:25:16 +0300

    Update TutuMobileTests.java

--- src/test/java/tests/TutuMobileTests.java
+++ src/test/java/tests/TutuMobileTests.java
@@ -9,78 +9,80 @@ import static io.qameta.allure.Allure.step;
 
 @Tag("TutuMobileTests")
 public class FindDifferentTicketsTests extends TestBase {
+ 
  @Test
  void openApp() {
  step("Open Tutu.ru app", () -> {
-     $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
-     $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
-     $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
-     $(AppiumBy.id("ru.tutu.tutu_emp:id/vLayerTopGradient")).shouldBe(visible);
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/vLayerTopGradient")).shouldBe(visible);
         });
       
   @Test
-   void FindTickets() {
-   step("Open Tutu.ru app", () -> {
-       $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
-       $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
-       $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
-       $(AppiumBy.id("ru.tutu.tutu_emp:id/vLayerTopGradient")).shouldBe(visible);
+  void FindTickets() {
+  step("Open Tutu.ru app", () -> {
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/vLayerTopGradient")).shouldBe(visible);
         });
          
-   step("FindTickets", () -> {
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/buttonHintTextView")).click();
-            $$(AppiumBy.className("android.view.ViewGroup")).get(2).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/buttonHintTextView")).click();
-            $$(AppiumBy.className("android.view.ViewGroup")).get(3).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/progress_button_layout")).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/tvTitle")).shouldHave(text("Moscow — Simferopol"));
+ step("FindTickets", () -> {
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/buttonHintTextView")).click();
+      $$(AppiumBy.className("android.view.ViewGroup")).get(2).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/buttonHintTextView")).click();
+      $$(AppiumBy.className("android.view.ViewGroup")).get(3).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/progress_button_layout")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/tvTitle")).shouldHave(text("Moscow — Simferopol"));
         });
         
-        
-  @Test
-    void FindFlightTickets() {
-        step("Open Tutu.ru app", () -> {
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/vLayerTopGradient")).shouldBe(visible);
-        });
-        
-        step("FindFlightTickets", () -> {
-            $$(AppiumBy.className("android.widget.ImageButton")).get(0).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/image_plane")).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/progress_button_layout")).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/tvTitle")).shouldHave(text("Moscow — Simferopol"));
-        });
-         @Test
-    void openAppAndFindTickets() {
-        step("Open Tutu.ru app", () -> {
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/vLayerTopGradient")).shouldBe(visible);
-        });
-        
-        step("FindTrainTickets", () -> {
-            $$(AppiumBy.className("android.widget.ImageButton")).get(0).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/image_train")).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/progress_button_layout")).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/tvTitle")).shouldHave(text("Moscow — Simferopol"));
-        });
  @Test
-    void openAppAndFindTickets() {
-        step("Open Tutu.ru app", () -> {
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/vLayerTopGradient")).shouldBe(visible);
+ void FindFlightTickets() {
+ step("Open Tutu.ru app", () -> {
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/vLayerTopGradient")).shouldBe(visible);
+        });
+        
+  step("FindFlightTickets", () -> {
+      $$(AppiumBy.className("android.widget.ImageButton")).get(0).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/image_plane")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/progress_button_layout")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/tvTitle")).shouldHave(text("Moscow — Simferopol"));
+        });
+ 
+ @Test
+ void openAppAndFindTickets() {
+ step("Open Tutu.ru app", () -> {
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/vLayerTopGradient")).shouldBe(visible);
+        });
+        
+ step("FindTrainTickets", () -> {
+      $$(AppiumBy.className("android.widget.ImageButton")).get(0).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/image_train")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/progress_button_layout")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/tvTitle")).shouldHave(text("Moscow — Simferopol"));
+        });
+ 
+  @Test
+ void openAppAndFindTickets() {
+ step("Open Tutu.ru app", () -> {
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/contRoot")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/vLayerTopGradient")).shouldBe(visible);
         });
        
-        step("FindAutobusTickets", () -> {
-            $$(AppiumBy.className("android.widget.ImageButton")).get(0).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/image_bus")).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/progress_button_layout")).click();
-            $(AppiumBy.id("ru.tutu.tutu_emp:id/tvTitle")).shouldHave(text("Moscow — Simferopol"));
+ step("FindAutobusTickets", () -> {
+      $$(AppiumBy.className("android.widget.ImageButton")).get(0).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/image_bus")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/progress_button_layout")).click();
+      $(AppiumBy.id("ru.tutu.tutu_emp:id/tvTitle")).shouldHave(text("Moscow — Simferopol"));
         });
     }
 }
